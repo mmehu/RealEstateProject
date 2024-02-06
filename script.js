74489d780f568ec7302da2dd5e3adf9607324d2e
Marikha Mehu <mmehu777@gmail.com>
2024-02-05 20:01:33 -0500

    attempting to add side menu

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 120);
 });
+
+
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');
+}
